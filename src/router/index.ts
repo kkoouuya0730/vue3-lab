fbbb0b84445deff9efed9922c2207863a4565162
康也 岩渕 <kouya.0730@icloud.com>
2023-12-29 13:53:32 +0900

    メニューリストの作成と各画面の作成

--- src/router/index.ts
+++ src/router/index.ts
@@ -18,6 +18,78 @@ const router = createRouter({
       meta: {
         requiresAuth: true
       }
+    },
+    {
+      path: '/scrap-manage',
+      name: 'ScrapManage',
+      component: () => import('../components/pages/ScrapManage.vue'),
+      props: true,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/article-manage',
+      name: 'ArticleManage',
+      component: () => import('../components/pages/ArticleManage.vue'),
+      props: true,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/book-manage',
+      name: 'BookManage',
+      component: () => import('../components/pages/BookManage.vue'),
+      props: true,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/github-deploy',
+      name: 'GitHubDeploy',
+      component: () => import('../components/pages/GitHubDeploy.vue'),
+      props: true,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/favorite-articles',
+      name: 'FavoriteArticle',
+      component: () => import('../components/pages/FavoriteArticle.vue'),
+      props: true,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/reading-book',
+      name: 'ReadingBook',
+      component: () => import('../components/pages/ReadingBook.vue'),
+      props: true,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/dashbord',
+      name: 'Dashbord',
+      component: () => import('../components/pages/DashBord.vue'),
+      props: true,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/account-settings',
+      name: 'AccountSettings',
+      component: () => import('../components/pages/AccountSetting.vue'),
+      props: true,
+      meta: {
+        requiresAuth: true
+      }
     }
   ]
 })
