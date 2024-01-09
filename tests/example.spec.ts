066710fe393e26949f4dd6429e7213e6bc18a71d
康也　岩渕 <kouya.0730@icloud.com>
2024-01-09 23:01:22 +0900

    e2eテストケース追加

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,20 +1,23 @@
 import { test, expect } from '@playwright/test'
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/')
-
+  await page.goto('http://localhost:5173/')
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/)
+  await expect(page).toHaveTitle(/Vite App/)
 })
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/')
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click()
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(
-    page.getByRole('heading', { name: 'Installation' })
-  ).toBeVisible()
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:5173/')
+  await page.getByRole('button', { name: 'Login' }).click()
+  await page.getByLabel('Email').click()
+  await page.getByLabel('Email').fill('kouya.0730@icloud.com')
+  await page.getByLabel('Password').click()
+  await page.getByLabel('Password').fill('kaorunagisa09')
+  await page.getByLabel('Sign-In').click()
+  await page
+    .locator('div')
+    .filter({ hasText: /^Sign-InCancel$/ })
+    .getByLabel('Sign-In')
+    .click()
+  await expect(page.getByRole('button', { name: '' })).toBeVisible()
 })
